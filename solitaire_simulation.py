cc4c08a6af84123ec93147e3ddbaa057323aa099
Maunil Mehta <maunilthemehta@gmail.com>
2026-07-03 15:47:39 -0400

    Updating strategies

--- solitaire_simulation.py
+++ solitaire_simulation.py
@@ -10,16 +10,10 @@ print(game.tableau)
 # Need to define player attributes and strategy choices
 
 # Strategy 1: Greedy player
-# This player will play cards as soon as a move becomes from the stock pile to the tableau or foundation piles
-# Look to play to tablaeu first, then look to play to foundation if can't play to foundation, then look to next card
+# Greedy which has 4 priorities in ranked order, move cards to foundation pile, expose face down cards in tableau, create empty columns, place kings in empty 
 
-
-# Strategy 2: Focus on revealing face down cards (+ Greedy strategy)
-# This player will look to try and move face up cards on the tableau to other available positions to be able to reveal face down cards
-# This will mean the player will have to evaluate each column face up card against the other columns (and the inverse relationship)
-# as well as against the current state of the foundation pile
-# If nothing can be moved on the tableau, then it turns attention to the stockpile
+# Strategy 2: Build Foundation  (+ Greedy strategy)
+# Foundation builder, which prioritizes building the foundation piles over everything else
 
 # Strategy 3: Look through and memorize the stockpile (+ Greedy strategy + revealing face down card)
-# This player will use it's first moves to look through the stockpile and take into account what cards are available
-# This strategy will combine all three of the strategies as well as moving kings to available columns
+# Tableau player, this prioritizes cleaning up the tableau and making moves there, last resort is to put cards in foundation piles
