194a9d1f040ba3c3176026fb1229280e02fc19a0
Maunil Mehta <maunilthemehta@gmail.com>
2026-07-27 22:18:00 -0700

    Debugging code, running multiple greedy simulations to recreate result being set to active

--- Solitaire.py
+++ Solitaire.py
@@ -897,38 +897,43 @@ if __name__ == "__main__":
     # Setup up of initial variables for simulation runs, plan is to run
     # three different strategies back to back to back. Will store required
     # info and output as a table
+    S = Solitaire('greedy', 0)
+    S = Solitaire('greedy', 1)
+    S = Solitaire('greedy', 2)
+    # S = Solitaire('greedy', 2)
+    print(S.result)
     
-    n = 10
-    winRate = 0
-    movesToWin = 0
-    strategies = ['greedy', 'foundation_first', 'random']
+    # n = 10
+    # winRate = 0
+    # movesToWin = 0
+    # strategies = ['greedy', 'foundation_first', 'random']
 
-    output_df = pd.DataFrame(columns = ['Strategy', 'winRate', 'avgMovesPerWin', 'avgMovesPerLoss'])
+    # output_df = pd.DataFrame(columns = ['Strategy', 'wins', 'losses', 'winRate', 'avgMovesPerWin', 'avgMovesPerLoss'])
 
-    data_df = pd.DataFrame(columns = ['Seed', 'Strategy', 'Result', 'moveCount', 'moveList'])
+    # data_df = pd.DataFrame(columns = ['Seed', 'Strategy', 'Result', 'moveCount', 'moveList'])
 
 
-    for strat in strategies:
-        wins = 0
-        losses = 0
-        moves_win = 0
-        moves_loss = 0
-        for i in range(n):
-            S = Solitaire(strat,i)
-            if S.result == "WON":
-                wins += 1
-                moves_win = moves_win + S.move_count
-            else:
-                losses += 1
-                moves_loss = moves_loss + S.move_count
-            data_df.loc[len(data_df)] = [S.seed, S.strategy, S.result, S.move_count, S.move_list]
+    # for strat in strategies:
+    #     wins = 0
+    #     losses = 0
+    #     moves_win = 0
+    #     moves_loss = 0
+    #     for i in range(n):
+    #         S = Solitaire(strat,i)
+    #         if S.result == "WON":
+    #             wins += 1
+    #             moves_win = moves_win + S.move_count
+    #         elif S.result == "LOST":
+    #             losses += 1
+    #             moves_loss = moves_loss + S.move_count
+    #         data_df.loc[len(data_df)] = [S.seed, S.strategy, S.result, S.move_count, S.move_list]
 
-        winRate = wins/n
-        avgMovesPerWin = moves_win/wins
-        avgMovesPerLoss = moves_loss/losses
-        output_df.loc[len(output_df)] = [S.strategy, winRate, avgMovesPerWin, avgMovesPerLoss]      
+    #     winRate = wins/n
+    #     avgMovesPerWin = moves_win/wins
+    #     avgMovesPerLoss = moves_loss/losses
+    #     output_df.loc[len(output_df)] = [S.strategy, winRate, avgMovesPerWin, avgMovesPerLoss]      
 
-    with pd.ExcelWriter('SolitaireData.xlsx', engine='openpyxl') as writer:
-        data_df.to_excel(writer, sheet_name='Move Data', index=False)
-        output_df.to_excel(writer, sheet_name='Output Data', index=False)
+    # with pd.ExcelWriter('SolitaireData.xlsx', engine='openpyxl') as writer:
+    #     data_df.to_excel(writer, sheet_name='Move Data', index=False)
+    #     output_df.to_excel(writer, sheet_name='Output Data', index=False)
     
